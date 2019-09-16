8ac9e6afff51b40e4ae16b3ca902b8cd18062397
vonjonmueller <vonjonmueller@gmail.com>
2019-09-15 23:04:37 -0700

    finished the boys up

--- Assign2/Assign2/MyStaticArray.cpp
+++ Assign2/Assign2/MyStaticArray.cpp
@@ -1,1 +1,86 @@
-#pragma once
+//Programmer: Von Mueller
+//Programmer's ID: 1735441
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#include "StaticArray.h"
+
+void input(StaticArray<double, 100>&);
+void display(StaticArray<double, 100>&);
+void lookup(StaticArray<double, 100>&);
+
+int main()
+{
+	std::cout << "Programmer: Von Mueller" << std::endl
+		<< "Programmer's ID: 1735441" << std::endl
+		<< "File: " << __FILE__ << std::endl;
+
+	StaticArray<double, 100> a;
+
+	input(a);
+	display(a);
+	lookup(a);
+
+	std::string pause;
+	std::cin >> pause;
+}
+
+void input(StaticArray<double, 100>& arr)
+{
+	while (true)
+	{
+		std::string inpOne, inpTwo;
+		std::cout << "Input an index and a value [Q to quit]: ";
+		std::cin >> inpOne >> inpTwo;
+		cin.ignore(1000, 10);
+
+		if (inpOne == "q" || inpOne == "Q")
+			return;
+
+		arr[atof(inpOne.c_str())] = atof(inpTwo.c_str());
+
+	}
+}
+
+void display(StaticArray<double, 100>& arr)
+{
+	int count = 0;
+
+	for (int i = 0; i < arr.capacity(); i++)
+		if (arr[i] != 0)
+			count++;
+
+	std::cout << "you stored this many varues: " << count << std::endl
+		<< "The endex value paris are: " << std::endl << std::endl;
+
+	for (int i = 0; i < arr.capacity(); i++)
+		if (arr[i] != 0)
+			std::cout << "\t" << i << "=> " << arr[i] << std::endl;
+	std::cout << std::endl << std::endl;
+
+}
+
+void lookup(StaticArray<double, 100>& arr)
+{
+	while (true)
+	{
+		std::string inpOne;
+		std::cout << "Input an index for me to look up [Q to quit]: ";
+		std::cin >> inpOne;
+		cin.ignore(1000, 10);
+
+		if (inpOne == "q" || inpOne == "Q")
+			return;
+
+		double index = atof(inpOne.c_str());
+
+		if ((arr[index] == 0) && (arr[index] != arr[-1]))
+			std::cout << "I didn't find it" << std::endl;
+		else
+			std::cout << "Found it -- the value stored at " << index << " is " << arr[index] << std::endl;
+
+	}
+}
--- Assign2/Assign2/StaticArray.TestDriver.cpp
+++ Assign2/Assign2/StaticArray.TestDriver.cpp
@@ -9,79 +9,238 @@ using namespace std;
 #include <cassert>
 #include "StaticArray.h"
 
+
+void output(std::string);
+
 int main()
 {
 	std::cout << "Programmer: Von Mueller" << std::endl
 		<< "Programmer's ID: 1735441" << std::endl
 		<< "File: " << __FILE__ << std::endl;
 
+
+	//START OF INT///////////////////////////////////////////////////////////////
 	StaticArray <int, 100> sI;
-	StaticArray <const int, 100> sIC;
-	StaticArray <char, 5> sC;
+	const int int1 = 12356;
+	const int int2 = 7654321;
+	const int int3 = 123123;
 
-	/*
-	std::cout << "\nTesting " << "" << std::endl
-		<< "Expected: " << "" << std::endl
-		<< "ACTUAL: " << << std::endl << std::endl;
-		assert();
-	*/
+	std::cout << "Integer Static Array test:" << std::endl
+		<< "-------------------------------" << std::endl;
 
-	std::cout << "\nTesting Array::Array\n" << std::endl;
+	//StaticArray::capacity()
+	std::cout << "Testing StaticArray::capacity()" << std::endl
+		<< "Expected: 100" << std::endl
+		<< "Actual: " << sI.capacity() << std::endl << std::endl;
+	assert(sI.capacity() == 100);
+
+	//StaticArray::Array default constructor
+	std::cout << "\nTesting StaticArray::StaticArray\n";
 	for (int i = 0; i < sI.capacity(); i++)
-	{
-		assert(sI[i] == 0);
-	}
+		assert(sI[i] == int());
+	std::cout << "Pass!\n\n";
+
+	//StaticArray::operator[] setter
+	sI[6] = int1;
+	std::cout << "\nTesting StaticArray::operator[] setter\n";
+	std::cout << "Expected: " << int1 << " for sI[6]\n";
+	std::cout << "Actual: " << sI[6] << std::endl << std::endl;
+	assert(sI[6] == int1);
+
+	sI[7] = int2;
+	std::cout << "Expected: " << int2 << " for sI[7]\n";
+	std::cout << "Actual: " << sI[7] << std::endl << std::endl;
+	assert(sI[7] == int2);
+
+	sI[-1000] = int3;
+	std::cout << "Expected: " << int3 << " for sI[-1000]\n";
+	std::cout << "Actual: " << sI[-1000] << std::endl << std::endl;
+	assert(sI[-1000] == int3);
 
 
-	std::cout << "\nTesting " << "StaticArray::capacity()" << std::endl
-		<< "EXPECTED: " << 100 << std::endl
-		<< "ACTUAL: " << sI.capacity << std::endl << std::endl;
-	assert(100 == sI.capacity);
+	assert(int1 == sI[6]);
+	assert(int2 == sI[7]);
+	assert(int3 == sI[-6]); // any out of range uses dummy
+	assert(int3 == sI[100]); // checks upper end of range
+	assert(int3 != sI[99]); //checks upper end of range
+	assert(int3 != sI[0]); // checks lower end of range
+
+
+	//StaticArray::Operator[] getter
+	std::cout << "\nTesting the StaticArray::operator[] getter\n";
+	const StaticArray<int, 100> sIC = sI;
+	for (int i = 0; i < sIC.capacity(); i++)
+		assert(sI[i] == sIC[i]);
+	std::cout << "Pass!\n\n";
+
+
+	//START OF DOUBLE////////////////////////////////////////////////
+	StaticArray <double, 100> sD;
+	const double double1 = 6.6;
+	const double double2 = 666.6;
+	const double double3 = 6666669.420;
+
+	std::cout << "Double Static Array test:" << std::endl
+		<< "-------------------------------" << std::endl;
+
+	//StaticArray::capacity()
+	std::cout << "Testing StaticArray::capacity()" << std::endl
+		<< "Expected: 100" << std::endl
+		<< "Actual: " << sD.capacity() << std::endl << std::endl;
+	assert(sD.capacity() == 100);
+
+	//StaticArray::Array default constructor
+	std::cout << "\nTesting StaticArray::StaticArray\n";
+	for (int i = 0; i < sD.capacity(); i++)
+		assert(sD[i] == double());
+	std::cout << "Pass!\n\n";
+
+	//StaticArray::operator[] setter
+	sD[6] = double1;
+	std::cout << "\nTesting StaticArray::operator[] setter\n";
+	std::cout << "Expected: " << double1 << " for sD[6]\n";
+	std::cout << "Actual: " << sD[6] << std::endl << std::endl;
+	assert(sD[6] == double1);
+
+	sD[7] = double2;
+	std::cout << "Expected: " << double2 << " for sD[7]\n";
+	std::cout << "Actual: " << sD[7] << std::endl << std::endl;
+	assert(sD[7] == double2);
+
+	sD[-1000] = double3;
+	std::cout << "Expected: " << double3 << " for sD[-1000]\n";
+	std::cout << "Actual: " << sD[-1000] << std::endl << std::endl;
+	assert(sD[-1000] == double3);
+
+
+	assert(double1 == sD[6]);
+	assert(double2 == sD[7]);
+	assert(double3 == sD[-6]); // any out of range uses dummy
+	assert(double3 == sD[100]); // checks upper end of range
+	assert(double3 != sD[99]); //checks upper end of range
+	assert(double3 != sD[0]); // checks lower end of range
+
+
+	//StaticArray::Operator[] getter
+	std::cout << "\nTesting the StaticArray::operator[] getter\n";
+	const StaticArray<double, 100> sDC = sD;
+	for (int i = 0; i < sDC.capacity(); i++)
+		assert(sD[i] == sDC[i]);
+	std::cout << "Pass!\n\n";
+
+
+	//START OF Character////////////////////////////////////////////////
+	StaticArray <char, 100> sC;
+	const char char1 = 's';
+	const char char2 = 'u';
+	const char char3 = 'h';
+
+	std::cout << "Character Static Array test:" << std::endl
+		<< "-------------------------------" << std::endl;
+
+	//StaticArray::capacity()
+	std::cout << "Testing StaticArray::capacity()" << std::endl
+		<< "Expected: 100" << std::endl
+		<< "Actual: " << sC.capacity() << std::endl << std::endl;
+	assert(sC.capacity() == 100);
+
+	//StaticArray::Array default constructor
+	std::cout << "\nTesting StaticArray::StaticArray\n";
+	for (int i = 0; i < sC.capacity(); i++)
+		assert(sC[i] == char());
+	std::cout << "Pass!\n\n";
+
+	//StaticArray::operator[] setter;
+	sC[6] = char1;
+	std::cout << "\nTesting StaticArray::operator[] setter\n";
+	std::cout << "Expected: " << char1 << " for sC[6]\n";
+	std::cout << "Actual: " << sC[6] << std::endl << std::endl;
+	assert(sC[6] == char1);
+
+	sC[7] = char2;
+	std::cout << "Expected: " << char2 << " for sC[7]\n";
+	std::cout << "Actual: " << sC[7] << std::endl << std::endl;
+	assert(sC[7] == char2);
+
+	sC[-1000] = char3;
+	std::cout << "Expected: " << char3 << " for sC[-1000]\n";
+	std::cout << "Actual: " << sC[-1000] << std::endl << std::endl;
+	assert(sC[-1000] == char3);
+
+
+	assert(char1 == sC[6]);
+	assert(char2 == sC[7]);
+	assert(char3 == sC[-6]); // any out of range uses dummy
+	assert(char3 == sC[100]); // checks upper end of range
+	assert(char3 != sC[99]); //checks upper end of range
+	assert(char3 != sC[0]); // checks lower end of range
+
+
+	//StaticArray::Operator[] getter
+	std::cout << "\nTesting the StaticArray::operator[] getter\n";
+	const StaticArray<char, 100> sCC = sC;
+	for (int i = 0; i < sCC.capacity(); i++)
+		assert(sC[i] == sCC[i]);
+	std::cout << "Pass!\n\n";
 
 	
+	//START OF String////////////////////////////////////////////////
+	StaticArray <string, 100> sS;
+	const string string1 = "Suh ";
+	const string string2 = "Dude";
+	const string string3 = "!!!!!!!!!!!!!!!!!!";
 
-	sI[5] = 69420;
-	//set
-	std::cout << "\nTesting " << "StaticArray::operator[](int)" << std::endl
-		<< "EXPECTED: " << 69420 << std::endl
-		<< "ACTUAL: " << sI[5] << std::endl << std::endl;
-	assert(69420 == sI[5]);
+	std::cout << "Character Static Array test:" << std::endl
+		<< "-------------------------------" << std::endl;
 
-	sI[-1000] = 8008135;
-	// out of range test
-	std::cout << "\nTesting " << "Assignment of out of range" << std::endl
-		<< "Expected: " << "8008135" << std::endl
-		<< "ACTUAL: " << sI[-1000] << std::endl << std::endl;
-	assert(8008135 == sI[-1000]);
+	//StaticArray::capacity()
+	std::cout << "Testing StaticArray::capacity()" << std::endl
+		<< "Expected: 100" << std::endl
+		<< "Actual: " << sS.capacity() << std::endl << std::endl;
+	assert(sS.capacity() == 100);
 
-	// Makes sure all out of range use dummy
-	std::cout << "\nTesting " << "All out of range return dummy" << std::endl
-		<< "Expected: " << "8008135" << std::endl
-		<< "ACTUAL: " << sI[-3] << std::endl << std::endl;
-	assert(8008135 == sI[-3]);
+	//StaticArray::Array default constructor
+	std::cout << "\nTesting StaticArray::StaticArray\n";
+	for (int i = 0; i < sS.capacity(); i++)
+		assert(sS[i] == std::string());
+	std::cout << "Pass!\n\n";
+
+	//StaticArray::operator[] setter;
+	sS[6] = string1;
+	std::cout << "\nTesting StaticArray::operator[] setter\n";
+	std::cout << "Expected: " << string1 << " for sS[6]\n";
+	std::cout << "Actual: " << sS[6] << std::endl << std::endl;
+	assert(sS[6] == string1);
+
+	sS[7] = string2;
+	std::cout << "Expected: " << string2 << " for sS[7]\n";
+	std::cout << "Actual: " << sS[7] << std::endl << std::endl;
+	assert(sS[7] == string2);
+
+	sS[-1000] = string3;
+	std::cout << "Expected: " << string3 << " for sS[-1000]\n";
+	std::cout << "Actual: " << sS[-1000] << std::endl << std::endl;
+	assert(sS[-1000] == string3);
 
 
-	std::cout << "\nTesting " << "sI[99] and sI[0] both are in range, thus not equal to dummy" << std::endl
-		<< "Expected: " << "0" << std::endl
-		<< "Actual: " << sI[99] << std::endl << std::endl;
-	assert((sI[99] != sI[-1]) && (sI[0] != sI[-1]));
+	assert(string1 == sS[6]);
+	assert(string2 == sS[7]);
+	assert(string3 == sS[-6]); // any out of range uses dummy
+	assert(string3 == sS[100]); // checks upper end of range
+	assert(string3 != sS[99]); //checks upper end of range
+	assert(string3 != sS[0]); // checks lower end of range
 
-	std::cout << "\nTesting " << "StaticArray::capacity()" << std::endl
-		<< "Expected: " << "5" << std::endl
-		<< "Actual: " << sC.capacity << std::endl << std::endl;
-	assert(5 == sC.capacity());
-	
-	sC[4] = 'h';
-	std::cout << "\nTesting " << "StaticArray::operator[]" << std::endl
-		<< "Expected: " << "'h'" << std::endl
-		<< "Actual: " << sC[4] << std::endl << std::endl;
-	assert('h' == sC[4]);
 
-	StaticArray <char, 5> sC2 = sC;
+	//StaticArray::Operator[] getter
+	std::cout << "\nTesting the StaticArray::operator[] getter\n";
+	const StaticArray<string, 100> sSC = sS;
+	for (int i = 0; i < sSC.capacity(); i++)
+		assert(sS[i] == sSC[i]);
+	std::cout << "Pass!\n\n";
+
 	std::string pause;
 	std::cin >> pause;
 }
-
 
 
 
